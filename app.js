1962b74e3bacbd1a5e5afe09a9c4b5f58ce2024a
RassuK <RassuK@users.noreply.github.com>
2018-03-26 18:13:40 +0300

    Scoreboard 2

--- app.js
+++ app.js
@@ -301,18 +301,27 @@ window.onload = function () {
 }
 
 function showScores () {
+
+  var table = document.getElementById('scoreTable')
+
     for(var i =0; i < localStorage.length; i++){
     
     var myObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
     var playerName = myObj["player"];
     var playerScore = myObj["score"];
-
-    var table = document.getElementById('scoreTable')
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = playerName
     cell2.innerHTML = playerScore
   }
+
+  var row = table.insertRow(0);
+  var headerCell = document.createElement("TH");
+  var headerCell2 = document.createElement("TH");
+  headerCell.innerHTML = "Player Name"
+  headerCell2.innerHTML = "Player Score"
+  row.appendChild(headerCell);
+  row.appendChild(headerCell2);
 }
